e4cf1ef20bc9d6a81e1afb1244e85a609dc0a90f
Emmaexcellent <emmaexcellent030@gmail.com>
2025-02-14 17:09:07 +0100

    first commit

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,13 @@
 import { AuthForm } from "@/components/auth/auth-form";
-import { getUser } from "@/lib/appwrite/auth";
-import { redirect } from "next/navigation";
+// import { getUser } from "@/lib/appwrite/auth";
+// import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
     
-  const user = await getUser();
-  if (user) {
-    redirect("/");
-  }
+  // const user = await getUser();
+  // if (user) {
+  //   redirect("/");
+  // }
 
   return (
     <div className="w-full max-w-sm md:max-w-3xl">
