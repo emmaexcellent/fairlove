18c04d45e1ff5b98b3c779798df2823ab1d8bd68
Emmaexcellent <emmaexcellent030@gmail.com>
2025-02-14 17:18:18 +0100

    first commit

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,16 +1,27 @@
 import { AuthForm } from "@/components/auth/auth-form";
-import { getUser } from "@/lib/appwrite/auth";
-import { redirect } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
+// import { getUser } from "@/lib/appwrite/auth";
+// import { redirect } from "next/navigation";
 
 export default async function SignUpPage() {
-  const user = await getUser();
-  if (user) {
-    redirect("/");
-  }
+  // const user = await getUser();
+  // if (user) {
+  //   redirect("/");
+  // }
 
   return (
     <div className="w-full max-w-sm md:max-w-3xl">
-      <AuthForm authPage="signup" />
+      <Suspense fallback={<LoadingFallback />}>
+        <AuthForm authPage="signup" />
+      </Suspense>
     </div>
   );
 }
+
+
+const LoadingFallback = () => (
+  <div className="w-full h-48 flex flex-col items-center justify-center">
+    <Loader2 size={50} className="animate-spin text-primary" />
+  </div>
+);
