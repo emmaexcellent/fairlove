45aa484d3bbcd3ea1661df8ce7546a83fa76b8dc
emmaexcellent <emmaexcellent030@gmail.com>
2025-12-17 21:13:40 +0100

    fix lint error

--- components/Features.tsx
+++ components/Features.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { Models } from "node-appwrite";
+import { Models } from "appwrite";
 
 const features = [
   {
