b54159cc1fa11ae6574dfa82e7d807a62a675bc6
emmaexcellent <emmaexcellent030@gmail.com>
2025-12-17 21:16:17 +0100

    fix lint error

--- components/gifts/gift-card.tsx
+++ components/gifts/gift-card.tsx
@@ -11,6 +11,7 @@ import { toast } from "sonner";
 interface GiftCardProps {
   gift: Gift;
   userCoins: number;
+  isOwned: boolean;
   onPurchase?: (giftId: string, cost: number, quantity: number) => boolean;
 }
 
